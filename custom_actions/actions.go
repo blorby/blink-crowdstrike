d70f43bf7034606324070bde581dcf0824e6a755
Shirley Gordon <shirley@shirleys-mbp.lan>
2022-02-10 13:17:06 +0200

    CU-23kdwgd: added custom actions

--- custom_actions/actions.go
+++ custom_actions/actions.go
@@ -1,1 +1,58 @@
-package custom_actions
+package actions
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	openapi_sdk "github.com/blinkops/blink-openapi-sdk/plugin"
+	customact "github.com/blinkops/blink-openapi-sdk/plugin/custom_actions"
+	"github.com/blinkops/blink-sdk/plugin"
+	"net/http"
+)
+
+const (
+	PluginName = "crowdstrike"
+	hostAgentIdParam = "Host Agent ID"
+)
+
+func GetCrowdStrikeCustomActions() customact.CustomActions {
+	actions := map[string]customact.ActionHandler{
+		"DeleteDevice": deleteDevice,
+	}
+
+	return customact.CustomActions{
+		Actions:           actions,
+		ActionsFolderPath: "custom_actions/actions",
+	}
+}
+
+func deleteDevice(ctx *plugin.ActionContext, request *plugin.ExecuteActionRequest) (*plugin.ExecuteActionResponse, error) {
+	requestUrl, err := openapi_sdk.GetRequestUrl(ctx, PluginName)
+	if err != nil {
+		return nil, errors.New("no request url provided")
+	}
+
+	params, err := request.GetParameters()
+	if err != nil {
+		return nil, err
+	}
+
+	id := params[hostAgentIdParam]
+
+	reqBody := json.RawMessage(`{
+  "ids": [
+    "`+ id +`"
+  ]
+}`)
+	marshalledReqBody, err := json.Marshal(&reqBody)
+	if err != nil {
+		return nil, errors.New("failed to marshal json")
+	}
+
+	url := requestUrl + "/devices/entities/devices-actions/v2?action_name=hide_host"
+	req, _ := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(marshalledReqBody))
+	req.Header.Set("Content-Type", "application/json")
+
+	return execRequest(ctx, req, request.Timeout)
+}
+
--- custom_actions/helpers.go
+++ custom_actions/helpers.go
@@ -1,15 +1,71 @@
 package actions
 
 import (
-"github.com/blinkops/blink-openapi-sdk/consts"
-openapi_sdk "github.com/blinkops/blink-openapi-sdk/plugin"
-"github.com/blinkops/blink-sdk/plugin"
-"net/http"
+	"encoding/json"
+	"errors"
+	"github.com/blinkops/blink-openapi-sdk/consts"
+	openapi_sdk "github.com/blinkops/blink-openapi-sdk/plugin"
+	"github.com/blinkops/blink-sdk/plugin"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strings"
 )
+
+const (
+	requestUrlParam   = "REQUEST_URL"
+	clientIdParam     = "Client ID"
+	clientSecretParam = "Client Secret"
+)
+
+func GetCrowdStrikeAccessToken(connection map[string]string, request *http.Request) error {
+	requestUrl := connection[requestUrlParam]
+
+	queryParams := url.Values{
+		"client_id":     {connection[clientIdParam]},
+		"client_secret": {connection[clientSecretParam]},
+	}
+
+	client := &http.Client{}
+
+	req, err := http.NewRequest(http.MethodPost, requestUrl+"/oauth2/token", strings.NewReader(queryParams.Encode()))
+	if err != nil {
+		return err
+	}
+
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	res, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	if res.StatusCode != http.StatusCreated {
+		return errors.New("invalid credentials")
+	}
+
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return err
+	}
+
+	var responseBody struct {
+		AccessToken string `json:"access_token"`
+	}
+
+	err = json.Unmarshal(body, &responseBody)
+	if err != nil {
+		return err
+	}
+	request.Header.Set("AUTHORIZATION", "Bearer " + responseBody.AccessToken)
+	return nil
+}
 
 func execRequest(ctx *plugin.ActionContext, request *http.Request, timeout int32) (*plugin.ExecuteActionResponse, error) {
 	res := &plugin.ExecuteActionResponse{ErrorCode: consts.OK}
-	response, err := openapi_sdk.ExecuteRequest(ctx, request, PluginName, Prefixes, HeaderAlias, timeout, nil)
+	response, err := openapi_sdk.ExecuteRequest(ctx, request, PluginName, nil, nil, timeout, GetCrowdStrikeAccessToken)
 	if err != nil {
 		return nil, err
 	}
